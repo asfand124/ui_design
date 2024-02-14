3e8644ba285e6a9d82775128d70871a27895beaf
abdullah <abdullahmad246@gmail.com>
2024-02-14 18:07:07 +0500

    admin and user navigator logic

--- lib/screen/Login.dart
+++ lib/screen/Login.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_design/screen/Home.dart';
 import 'package:ui_design/screen/Signup.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -16,33 +17,60 @@ TextEditingController _pass = TextEditingController();
 class _LoginState extends State<Login> {
   @override
   void initState() {
-    checkuser();
+    // checkuser();
   }
 
-  checkuser() async {
-    if (FirebaseAuth.instance.currentUser != null) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.push(
-          context as BuildContext,
-          MaterialPageRoute(builder: (context) => const Home()),
-        );
-      });
+  NavigateUser(String title) {
+    switch (title) {
+      case "user":
+        {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: ((context) => Home())));
+          break;
+        }
+      case "admin":
+        {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: ((context) => Home())));
+          break;
+        }
     }
   }
+
+  // checkuser() async {
+  //   if (FirebaseAuth.instance.currentUser != null) {
+  //     WidgetsBinding.instance.addPostFrameCallback((_) {
+  //       // Navigator.push(
+  //       //   context as BuildContext,
+  //       //   MaterialPageRoute(builder: (context) => const Home()),
+  //       // );
+  //     });
+  //   }
+  // }
 
   Signin() async {
     try {
       FirebaseAuth.instance
           .signInWithEmailAndPassword(email: _email.text, password: _pass.text)
-          .then((value) {
-        Navigator.push(
-          context as BuildContext,
-          MaterialPageRoute(builder: (context) => const Home()),
-        );
+          .then((res) {
+        // print(res.user?.uid);
+        getUserType(res.user?.uid);
       });
     } catch (e) {
       print(e);
     }
+  }
+
+  getUserType([String? uid]) {
+    final docRef = FirebaseFirestore.instance.collection("Users").doc(uid);
+    docRef.get().then(
+      (doc) {
+        Map<String, dynamic> userType = doc.data()!;
+        final type = userType["Type"];
+        NavigateUser(type);
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
   }
 
   @override
